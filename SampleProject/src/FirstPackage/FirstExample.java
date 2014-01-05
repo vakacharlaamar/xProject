5dfda225c8c5e2e4763c917eba4b3599b135bef3
vakacharlaamar <vakacharla.satyaamar@gmail.com>
2014-01-05 14:55:44 -0800

    Update FirstExample.java
    Adding changes to Hello Example

--- SampleProject/src/FirstPackage/FirstExample.java
+++ SampleProject/src/FirstPackage/FirstExample.java
@@ -2,6 +2,6 @@ package FirstPackage;
 
 public class FirstExample {
 	public static void main(String[] args) {
-		System.out.println("This is hello world example");
+		System.out.println("Changes made to Hello Example");
 	}
 }
